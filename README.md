7435b16c3e151b147e015c347f8a611a99e75bd3
trujunzhang <trujunzhang@gmail.com>
2016-06-17 20:56:09 +0800

    mzadalny.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# mzadalny
+# Create mysql database
+
+create database mzadalny;
+
+grant all on mzadalny.* to 'mzada' identified by 'mzada720';
+
+mysql -u mzada -p 'mzada720' mzadalny
---
+++ include/config.php
@@ -0,0 +1,21 @@
+<?PHP
+
+$user_name = "mzada";
+$password = "mzada720";
+$database = "mzadalny";
+$server = "127.0.0.1";
+
+$mysqli = new mysqli($server, $user_name, $password, $database);
+
+/*
+ * This is the "official" OO way to do it,
+ * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
+ */
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+        . $mysqli->connect_error);
+} else {
+    print "Connection to the Server opened";
+}
+
+?>
