06bdc871886192ce9c7f7b1b095a3c6b84429e62
trujunzhang <trujunzhang@gmail.com>
2016-06-18 21:52:06 +0800

    xxx

---
+++ db/query.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `ads` WHERE close_ads = "0" and type_ads_or = "1" ORDER BY Last_updated_Ad DESC;
+
+
+SELECT * FROM `ads` WHERE close_ads = "0" and type_ads_or = "1" and fixed_home = "1" ORDER BY Last_updated_Ad DESC
