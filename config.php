5ebcd1b4cd4046b2c62ed00a2c1b7766f9fec34f
trujunzhang <trujunzhang@gmail.com>
2016-06-23 19:07:50 +0800

    xxx

---
+++ config.php
@@ -0,0 +1,24 @@
+<?PHP
+
+$url_hraj = "http://www.mzadalny.com/";
+
+$user_name = "b32_15129989";
+$password = "wanghao720";
+$database = "b32_15129989_mzadalny";
+$server = "sql206.byethost32.com";
+
+
+$mysqli = new mysqli($server, $user_name, $password, $database);
+
+/*
+ * This is the "official" OO way to do it,
+ * BUT $connect_error was broken until PHP 5.2.9 and 5.3.0.
+ */
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+        . $mysqli->connect_error);
+} else {
+//    print "Connection to the Server opened";
+}
+
+?>
