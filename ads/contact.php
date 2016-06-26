aba74ed61431bf830398b9d7f7cf588b5dd76352
trujunzhang <trujunzhang@gmail.com>
2016-06-26 14:08:14 +0800

    xxx

--- ads/contact.php
+++ ads/contact.php
@@ -1,6 +1,19 @@
 <div class="contact">
 
     <?php if ($time_add_ads + (60 * 60 * 24 * 60) > time()) { ?>
+        <?php
+        $is_opensooq_contact = false;
+        if (strpos($ads_contact, 'base64') !== false) {
+            $is_opensooq_contact = true;
+            $opensooq_phone_id = explode(",", $ads_contact)[1];
+            mysqli_query($mysqli, "SET NAMES 'utf8'");
+            $query_keyinformation_print = "SELECT * FROM `ads` WHERE id = \"$opensooq_phone_id\" ";
+            $result_keyinformation_print = mysqli_query($mysqli, $query_keyinformation_print);
+            $print_value_opensooq_phone = mysqli_fetch_array($result_keyinformation_print);
+            $ads_contact = $print_value_opensooq_phone["phone"];
+        }
+
+        ?>
         <span class="label label-success">وسيلة الإتصال :</span>
         <strong>
             <?php echo $ads_contact; ?>
